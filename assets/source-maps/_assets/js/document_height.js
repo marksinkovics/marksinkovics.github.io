e2c76a9b01b0e1c0827dccc9f19963a5f73c7af2
marksinkovics <marksinkovics@users.noreply.github.com>
2023-06-16 18:56:33 +0000

    jekyll build from Action 10dd48b6e6cb1b06d2ad209e39c45a91e9365dbb

---
+++ assets/source-maps/_assets/js/document_height.js
@@ -0,0 +1,12 @@
+function setDocHeight() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+};
+
+window.addEventListener('resize', function () {
+    setDocHeight();
+});
+window.addEventListener('orientationchange', function () {
+    setDocHeight();
+});
+
+setDocHeight();
